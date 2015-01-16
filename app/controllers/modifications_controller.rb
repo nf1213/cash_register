efc2f82ff567f588add08e92b62d144c0555ebb4
Nicole <nfelch121394@gmail.com>
2015-01-16 15:29:47 -0500

    Modification update destroy

--- app/controllers/modifications_controller.rb
+++ app/controllers/modifications_controller.rb
@@ -3,12 +3,35 @@ class ModificationsController < ApplicationController
   before_action :authenticate
   before_action :must_be_manager
 
+  def index
+    @modifications = Modification.all
+  end
+
+  def edit
+    @modification = Modification.find(params[:id])
+  end
+
+  def update
+    @modification = Modification.find(params[:id])
+    @modification.update(modification_params)
+    price = params[:modification][:price].to_f * 100
+    @modification.update(price: price)
+    redirect_to modifications_path, notice: "Modification Updated"
+  end
+
+  def destroy
+    @modification = Modification.find(params[:id]).destroy
+    redirect_to modifications_path, notice: "Modification destroyed"
+  end
+
   def new
     @modification = Modification.new
   end
 
   def create
     @modification = Modification.new(modification_params)
+    price = params[:modification][:price].to_f * 100
+    @modification.price = price
     if @modification.save
       redirect_to static_pages_manager_functions_path, notice: "New modification created"
     else
@@ -19,6 +42,6 @@ class ModificationsController < ApplicationController
   private
 
   def modification_params
-    params.require(:modification).permit(:name)
+    params.require(:modification).permit(:name, :price)
   end
 end
